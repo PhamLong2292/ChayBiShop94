9b53a255371c3879061108e7c714c52c9b73bd42
PhamLong2292 <Phamlong2292@gmail.com>
2023-02-23 14:32:33 +0700

    upload

---
+++ ChayBiShop94/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChayBiShop94.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
