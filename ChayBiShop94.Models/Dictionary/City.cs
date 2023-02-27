4a31e78d1098384b1c297ec4148cd8ccbc078ee6
PhamLong2292 <Phamlong2292@gmail.com>
2023-02-27 17:26:44 +0700

    Upload

---
+++ ChayBiShop94.Models/Dictionary/City.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChayBiShop94.Models
+{
+    public class City
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(3)]
+        public string Code { get; set; }
+
+        [Required]
+        [MaxLength(75)]
+        public string Name { get; set; }
+
+        [ForeignKey("Country")]
+        public int CountryId { get; set; }
+
+        public virtual Country Country { get; set; }
+    }
+}
